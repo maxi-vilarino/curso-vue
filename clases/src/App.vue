ac9114b1fccf4b32179a92a46edf44f00a665a44
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-04-30 12:26:33 +0200

    feat: reorganizar App.vue con contenedor y funcionalidad de contador

--- clases/src/App.vue
+++ clases/src/App.vue
@@ -1,10 +1,34 @@
 <script setup>
+import { ref } from "vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
+
+let number = ref(0);
+
+const increment = () => {
+  number.value++;
+};
+
+const myId = "some-id";
+const myValue = ref("some value");
 </script>
 
 <template>
   <HeaderComponent />
-  <h1>Hola Mundo!!!!</h1>
+  <div class="container">
+    <h1 :id="myId">Hola Mundo!!!!</h1>
+    <p>Contador: {{ number }}</p>
+    <button @click="increment">Incrementar</button>
+    <input type="text" :value="myValue" />
+  </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  height: 100vh;
+}
+</style>
