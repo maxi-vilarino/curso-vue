8c50ef395cb494e70da94ef79a9cf312b9e2c0ae
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-04-30 12:39:48 +0200

    feat: agregar lógica de condiciones y resaltar título en App.vue

--- clases/src/App.vue
+++ clases/src/App.vue
@@ -10,15 +10,24 @@ const increment = () => {
 
 const myId = "some-id";
 const myValue = ref("some value");
+
+const condition = false;
+const anotherCondition = false;
+
+let myCondition = true;
 </script>
 
 <template>
   <HeaderComponent />
   <div class="container">
-    <h1 :id="myId">Hola Mundo!!!!</h1>
+    <h1 :id="myId" :class="myCondition ? 'highlight' : ''">Hola Mundo!!!!</h1>
+    <h2 v-if="condition">La condición se cumple</h2>
+    <h2 v-else-if="anotherCondition">Otra condición se cumple</h2>
+    <h2 v-else>No se cumple ninguna condición</h2>
     <p>Contador: {{ number }}</p>
     <button @click="increment">Incrementar</button>
-    <input type="text" :value="myValue" />
+    <h2>{{ myValue }}</h2>
+    <input type="text" v-model="myValue" />
   </div>
 </template>
 
@@ -30,5 +39,10 @@ const myValue = ref("some value");
   justify-content: center;
   gap: 1rem;
   height: 100vh;
+}
+
+.highlight {
+  color: red;
+  font-weight: bold;
 }
 </style>
